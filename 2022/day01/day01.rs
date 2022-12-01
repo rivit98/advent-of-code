5a5b2b10fd0c58952988babf87d23b4783796d52
Albert Gierlach <agierlac@akamai.com>
2022-12-01 12:05:16 +0100

    day1 refactor, lol

--- 2022/day01/day01.rs
+++ 2022/day01/day01.rs
@@ -1,32 +1,11 @@
-use std::fs::File;
-use std::io::BufRead;
-use std::io::BufReader;
-
-
-use itertools::Itertools;
-
 fn main() {
-    let lines = read_lines("./input.txt");
-    let groups = lines.iter()
-        .group_by(|l| (**l).len() == 0)
-        .into_iter()
-        .filter(|(ge0, _)| *ge0 == false)
-        .map(|(_, group)| group.cloned().map(|line| line.parse::<u64>().unwrap()).collect())
-        .collect::<Vec<Vec<u64>>>();
-
-    let sums: Vec<u64> = groups.iter()
-        .map(|g| g.iter().sum())
+    let mut sums: Vec<u64> = include_str!("./input.txt")
+        .split("\n\n")
+        .map(|group| group.lines().map(|line| line.parse::<u64>().unwrap()).sum())
         .collect();
 
+    sums.sort();
+
     println!("part1 {}", sums.iter().max().unwrap());
-    println!("part2 {}", sums.iter().sorted().rev().take(3).sum::<u64>())
-
-}
-
-fn read_lines(filename: &str) -> Vec<String> {
-    let f = File::open(filename).unwrap();
-    let reader = BufReader::new(f);
-    reader.lines()
-        .map(|l| l.unwrap())
-        .collect()
+    println!("part2 {}", sums.iter().rev().take(3).sum::<u64>())
 }
