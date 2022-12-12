ebd49f1e1a7946aaee33856a28ec189928a3f9f9
Albert Gierlach <agierlac@akamai.com>
2022-12-12 16:44:03 +0100

    day11 sync

--- 2022/day11/day11.rs
+++ 2022/day11/day11.rs
@@ -1,3 +1,30 @@
+use std::{collections::HashMap, vec};
+
+struct Item {
+    val: usize,
+    vals: HashMap<usize, usize>,
+}
+
+impl Item {
+    pub fn new(val: usize) -> Self {
+        Item {
+            val: val,
+            vals: HashMap::new(),
+        }
+    }
+    pub fn from_vals(vals: HashMap<usize, usize>) -> Self {
+        Item {
+            val: 0,
+            vals: vals,
+        }
+    }
+
+    fn update(&mut self, val: usize, divisors: &Vec<usize>) {
+        self.vals = HashMap::from_iter(
+            divisors.iter().map(|&divisor| (divisor, val % divisor))
+        )
+    }
+}
 
 struct Test {
     div: usize,
@@ -6,7 +33,7 @@ struct Test {
 }
 
 struct Monkey<'a> {
-    items: Vec<usize>,
+    items: Vec<Item>,
     op_tok: Vec<&'a str>,
     test: Test,
     inspections: usize,
@@ -17,7 +44,7 @@ impl<'a> Monkey<'a> {
         let a = if self.op_tok[0] == "old" { worry_level } else {self.op_tok[0].parse::<usize>().unwrap()};
         let b = if self.op_tok[2] == "old" { worry_level } else {self.op_tok[2].parse::<usize>().unwrap()};
         
-        return if self.op_tok[1] == "+" { a + b} else {a * b};
+        return if self.op_tok[1] == "+" { a + b } else {a * b};
     }
     fn dest(&self, worry_level: usize) -> usize {
         if worry_level % self.test.div == 0 {
@@ -36,7 +63,11 @@ fn main() {
     let mut monkeys: Vec<Monkey> = data.iter()
         .map(|chunk| {
             let lines: Vec<&str> = chunk.lines().collect();
-            let starting_items: Vec<usize> = lines[1].split_once(':').unwrap().1.split(",").map(|x| x.trim().parse::<usize>().unwrap()).collect();
+            let starting_items: Vec<Item> = lines[1].split_once(':').unwrap().1.split(",")
+                .map(|x| x.trim().parse::<usize>().unwrap())
+                .map(|x| Item::new(x))
+                .collect();
+            
             let operands: Vec<&str> = lines[2].split_once('=').unwrap().1.split_whitespace().collect();
 
             Monkey { 
@@ -51,15 +82,15 @@ fn main() {
             }
         })
         .collect();
-
+    
     for _ in 1..=20 {
         for midx in 0..monkeys.len() {
             for idx in 0..monkeys[midx].items.len() {
                 monkeys[midx].inspections += 1;
-                let item = monkeys[midx].items[idx];
-                let new_worry_level = monkeys[midx].operate(item) / 3;
+                let item = &monkeys[midx].items[idx];
+                let new_worry_level = monkeys[midx].operate(item.val) / 3;
                 let target = monkeys[midx].dest(new_worry_level);
-                monkeys[target].items.push(new_worry_level);
+                monkeys[target].items.push(Item::new(new_worry_level));
             }
 
             monkeys[midx].items.clear();
@@ -68,7 +99,36 @@ fn main() {
 
     let mut part1: Vec<usize> = monkeys.iter().map(|m| m.inspections).collect();
     part1.sort();
-    println!("part1 {}", part1.iter().rev().take(2).product::<usize>())
+    println!("part1 {}", part1.iter().rev().take(2).product::<usize>());
 
+    let divisors: Vec<usize> = monkeys.iter().map(|m| m.test.div).collect();
+
+    // monkeys.iter_mut().for_each(|m| {
+    //     m.items = m.items.iter()
+    //     .map(|item| {
+    //         let vals = HashMap::from_iter(
+    //             divisors.iter().map(|&divisor| (divisor, item.val % divisor)).collect::<Vec<(usize, usize)>>()
+    //         );
+    //         return Item::from_vals(vals);
+    //     })
+    //     .collect();
+    // });
+
+    for _ in 1..=20 {
+        for midx in 0..monkeys.len() {
+            for idx in 0..monkeys[midx].items.len() {
+                monkeys[midx].inspections += 1;
+
+                let item = &monkeys[midx].items[idx];
+                let item_val = *item.vals.get(&monkeys[midx].test.div).unwrap();
+                let new_worry_level = monkeys[midx].operate(item_val);
+                item.update(new_worry_level, &divisors);
+                let target = monkeys[midx].dest(*item.vals.get(&monkeys[midx].test.div).unwrap());
+                monkeys[target].items.push(Item::new(new_worry_level));
+            }
+
+            monkeys[midx].items.clear();
+        }
+    }
 }
 
